2299889a1df81a24eb305de903f27cb9286610c3
Yanis <Yanis@YANIS-PC>
2022-03-05 16:28:10 +0100

    Revert "Navigation"
    This reverts commit 3d30f16a139b7884e85c7482012392011533cf8d.

--- gtsco2/forms/Navigation.cs
+++ gtsco2/forms/Navigation.cs
@@ -112,10 +112,5 @@ namespace gtsco2.forms
             fr.refrech();
             fr.ShowDialog();
         }
-
-        private void Navigation_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }

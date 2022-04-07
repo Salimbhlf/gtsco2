7f35f52b8e6273a6e2022b4aa88be2b57aae14d6
salim bhlf <salimbhlf@gmail.com>
2022-04-07 01:12:54 +0100

    avonene chanegemant

---
+++ gtsco2/forms/Avnone/AvonenChanegement.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace gtsco2.forms.Avnone
+{
+    public partial class AvonenChanegement : DevExpress.XtraReports.UI.XtraReport
+    {
+        public AvonenChanegement()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
